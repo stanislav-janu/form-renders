88a174069918140bbf234996750596b2af73e838
Stanislav Janů <info@lweb.cz>
2017-08-24 14:03:16 +0200

    BootstrapFullWightFormRender

---
+++ src/BootstrapFullWightFormRender.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stanley
+ * Date: 20.11.15
+ * Time: 18:27
+ */
+
+namespace JCode;
+
+use Nette,
+	Nette\Forms\Controls;
+
+class BootstrapFullWightFormRender extends Nette\Forms\Rendering\DefaultFormRenderer
+{
+	public $wrappers = array(
+		'form' => array(
+			'container' => NULL,
+		),
+
+		'error' => array(
+			'container' => 'div class="alert alert-danger" role=alert',
+			'item' => 'p',
+		),
+
+		'group' => array(
+			'container' => 'fieldset',
+			'label' => 'legend',
+			'description' => 'p',
+		),
+
+		'controls' => array(
+			'container' => NULL,
+		),
+
+		'pair' => array(
+			'container' => 'div class=form-group',
+			'.required' => 'required',
+			'.optional' => NULL,
+			'.odd' => NULL,
+			'.error' => 'has-error',
+		),
+
+		'control' => array(
+			'container' => 'div class=col-sm-12',
+			'.odd' => NULL,
+
+			'description' => 'span class=help-block',
+			'requiredsuffix' => '',
+			'errorcontainer' => 'span class=help-block',
+			'erroritem' => '',
+
+			'.required' => 'required',
+			'.text' => 'text',
+			'.password' => 'text',
+			'.file' => 'text',
+			'.submit' => 'button',
+			'.image' => 'imagebutton',
+			'.button' => 'button',
+		),
+
+		'label' => array(
+			'container' => 'div class="col-sm-12 control-label" style="text-align: left"',
+			'suffix' => NULL,
+			'requiredsuffix' => '',
+		),
+
+		'hidden' => array(
+			'container' => NULL,
+		),
+	);
+
+	public function render(Nette\Forms\Form $form, $mode = NULL)
+	{
+		$form->getElementPrototype()->class('form-horizontal');
+		foreach ($form->getControls() as $control)
+		{
+			if ($control instanceof Controls\Button)
+			{
+				$control->setAttribute('class', empty($usedPrimary) ? 'btn btn-primary' : 'btn btn-default');
+				$usedPrimary = TRUE;
+
+			}
+			elseif ($control instanceof Controls\TextBase || $control instanceof Controls\SelectBox || $control instanceof Controls\MultiSelectBox)
+			{
+				$control->setAttribute('class', 'form-control');
+				if($control instanceof Controls\TextBase)
+				{
+					$type = $control->getControlPrototype()->getAttribute('type');
+					if($type == 'datetime')
+					{
+						$control->setAttribute('data-onload-datetimepicker', '{"locale": "cs", "format": "YYYY-MM-DD HH:mm"}');
+					}
+					elseif($type == 'date')
+					{
+						$control->setAttribute('data-onload-datetimepicker', '{"locale": "cs", "format": "YYYY-MM-DD"}');
+					}
+					elseif($type == 'time')
+					{
+						$control->setAttribute('data-onload-datetimepicker', '{"locale": "cs", "format": "HH:mm"}');
+					}
+				}
+
+			}
+			elseif ($control instanceof Controls\Checkbox || $control instanceof Controls\CheckboxList || $control instanceof Controls\RadioList)
+			{
+				$control->getSeparatorPrototype()->setName('div')->class($control->getControlPrototype()->type);
+			}
+		}
+		return parent::render($form, $mode);
+	}
+}
